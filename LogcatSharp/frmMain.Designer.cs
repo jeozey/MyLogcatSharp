99d660c313403ac4d7f1bd30f3d7afef729f8102
jeozey <18960868232@189.cn>
2017-10-20 15:06:03 +0800

    add mutil devices choose,log level filter

--- LogcatSharp/frmMain.Designer.cs
+++ LogcatSharp/frmMain.Designer.cs
@@ -28,139 +28,164 @@
 		/// </summary>
 		private void InitializeComponent()
 		{
-			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
-			this.textAdb = new System.Windows.Forms.TextBox();
-			this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-			this.toolStripButtonStart = new System.Windows.Forms.ToolStripButton();
-			this.toolStripButtonStop = new System.Windows.Forms.ToolStripButton();
-			this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
-			this.toolStripButtonClear = new System.Windows.Forms.ToolStripButton();
-			this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
-			this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
-			this.toolStripTextBoxFilter = new System.Windows.Forms.ToolStripTextBox();
-			this.toolStripComboBoxFilterType = new System.Windows.Forms.ToolStripComboBox();
-			this.toolStrip1.SuspendLayout();
-			this.SuspendLayout();
-			// 
-			// textAdb
-			// 
-			this.textAdb.BackColor = System.Drawing.SystemColors.ControlLightLight;
-			this.textAdb.Dock = System.Windows.Forms.DockStyle.Fill;
-			this.textAdb.Location = new System.Drawing.Point(0, 25);
-			this.textAdb.Multiline = true;
-			this.textAdb.Name = "textAdb";
-			this.textAdb.ReadOnly = true;
-			this.textAdb.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-			this.textAdb.Size = new System.Drawing.Size(667, 353);
-			this.textAdb.TabIndex = 0;
-			this.textAdb.WordWrap = false;
-			// 
-			// toolStrip1
-			// 
-			this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripButtonStart,
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.outputAdb = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.deviceSelect = new System.Windows.Forms.ToolStripComboBox();
+            this.btnStart = new System.Windows.Forms.ToolStripButton();
+            this.toolStripButtonStop = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnClear = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.txtFilter = new System.Windows.Forms.ToolStripTextBox();
+            this.regSelect = new System.Windows.Forms.ToolStripComboBox();
+            this.levelSelect = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // outputAdb
+            // 
+            this.outputAdb.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.outputAdb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.outputAdb.Location = new System.Drawing.Point(0, 25);
+            this.outputAdb.Name = "outputAdb";
+            this.outputAdb.ReadOnly = true;
+            this.outputAdb.Size = new System.Drawing.Size(667, 324);
+            this.outputAdb.TabIndex = 0;
+            this.outputAdb.Text = "";
+            this.outputAdb.WordWrap = false;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.deviceSelect,
+            this.btnStart,
             this.toolStripButtonStop,
             this.toolStripSeparator2,
-            this.toolStripButtonClear,
+            this.btnClear,
             this.toolStripSeparator1,
             this.toolStripLabel1,
-            this.toolStripTextBoxFilter,
-            this.toolStripComboBoxFilterType});
-			this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-			this.toolStrip1.Name = "toolStrip1";
-			this.toolStrip1.Size = new System.Drawing.Size(667, 25);
-			this.toolStrip1.TabIndex = 1;
-			this.toolStrip1.Text = "toolStrip1";
-			// 
-			// toolStripButtonStart
-			// 
-			this.toolStripButtonStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButtonStart.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonStart.Image")));
-			this.toolStripButtonStart.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButtonStart.Name = "toolStripButtonStart";
-			this.toolStripButtonStart.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButtonStart.Text = "Start";
-			this.toolStripButtonStart.Click += new System.EventHandler(this.toolStripButtonStart_Click);
-			// 
-			// toolStripButtonStop
-			// 
-			this.toolStripButtonStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
-			this.toolStripButtonStop.Enabled = false;
-			this.toolStripButtonStop.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonStop.Image")));
-			this.toolStripButtonStop.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButtonStop.Name = "toolStripButtonStop";
-			this.toolStripButtonStop.Size = new System.Drawing.Size(23, 22);
-			this.toolStripButtonStop.Text = "Stop";
-			this.toolStripButtonStop.Click += new System.EventHandler(this.toolStripButtonStop_Click);
-			// 
-			// toolStripSeparator2
-			// 
-			this.toolStripSeparator2.Name = "toolStripSeparator2";
-			this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
-			// 
-			// toolStripButtonClear
-			// 
-			this.toolStripButtonClear.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonClear.Image")));
-			this.toolStripButtonClear.ImageTransparentColor = System.Drawing.Color.Magenta;
-			this.toolStripButtonClear.Name = "toolStripButtonClear";
-			this.toolStripButtonClear.Size = new System.Drawing.Size(54, 22);
-			this.toolStripButtonClear.Text = "Clear";
-			this.toolStripButtonClear.Click += new System.EventHandler(this.toolStripButton1_Click);
-			// 
-			// toolStripSeparator1
-			// 
-			this.toolStripSeparator1.Name = "toolStripSeparator1";
-			this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
-			// 
-			// toolStripLabel1
-			// 
-			this.toolStripLabel1.Name = "toolStripLabel1";
-			this.toolStripLabel1.Size = new System.Drawing.Size(36, 22);
-			this.toolStripLabel1.Text = "Filter:";
-			// 
-			// toolStripTextBoxFilter
-			// 
-			this.toolStripTextBoxFilter.Name = "toolStripTextBoxFilter";
-			this.toolStripTextBoxFilter.Size = new System.Drawing.Size(100, 25);
-			// 
-			// toolStripComboBoxFilterType
-			// 
-			this.toolStripComboBoxFilterType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-			this.toolStripComboBoxFilterType.Items.AddRange(new object[] {
+            this.txtFilter,
+            this.regSelect,
+            this.levelSelect});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(667, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // deviceSelect
+            // 
+            this.deviceSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.deviceSelect.Name = "deviceSelect";
+            this.deviceSelect.Size = new System.Drawing.Size(105, 25);
+            // 
+            // btnStart
+            // 
+            this.btnStart.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnStart.Image = ((System.Drawing.Image)(resources.GetObject("btnStart.Image")));
+            this.btnStart.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(23, 22);
+            this.btnStart.Text = "Start";
+            this.btnStart.Click += new System.EventHandler(this.toolStripButtonStart_Click);
+            // 
+            // toolStripButtonStop
+            // 
+            this.toolStripButtonStop.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.toolStripButtonStop.Enabled = false;
+            this.toolStripButtonStop.Image = ((System.Drawing.Image)(resources.GetObject("toolStripButtonStop.Image")));
+            this.toolStripButtonStop.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButtonStop.Name = "toolStripButtonStop";
+            this.toolStripButtonStop.Size = new System.Drawing.Size(23, 22);
+            this.toolStripButtonStop.Text = "Stop";
+            this.toolStripButtonStop.Click += new System.EventHandler(this.toolStripButtonStop_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Image = ((System.Drawing.Image)(resources.GetObject("btnClear.Image")));
+            this.btnClear.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(58, 22);
+            this.btnClear.Text = "Clear";
+            this.btnClear.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(39, 22);
+            this.toolStripLabel1.Text = "Filter:";
+            // 
+            // txtFilter
+            // 
+            this.txtFilter.Name = "txtFilter";
+            this.txtFilter.Size = new System.Drawing.Size(100, 25);
+            // 
+            // regSelect
+            // 
+            this.regSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.regSelect.Items.AddRange(new object[] {
             "Simple",
             "Regex"});
-			this.toolStripComboBoxFilterType.Name = "toolStripComboBoxFilterType";
-			this.toolStripComboBoxFilterType.Size = new System.Drawing.Size(75, 25);
-			// 
-			// frmMain
-			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(667, 378);
-			this.Controls.Add(this.textAdb);
-			this.Controls.Add(this.toolStrip1);
-			this.Name = "frmMain";
-			this.Text = "Logcat #";
-			this.Load += new System.EventHandler(this.frmMain_Load);
-			this.toolStrip1.ResumeLayout(false);
-			this.toolStrip1.PerformLayout();
-			this.ResumeLayout(false);
-			this.PerformLayout();
+            this.regSelect.Name = "regSelect";
+            this.regSelect.Size = new System.Drawing.Size(75, 25);
+            // 
+            // levelSelect
+            // 
+            this.levelSelect.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.levelSelect.Items.AddRange(new object[] {
+            "Verbose",
+            "Debug",
+            "Info",
+            "Warn",
+            "Error",
+            "Assert"});
+            this.levelSelect.Name = "levelSelect";
+            this.levelSelect.Size = new System.Drawing.Size(75, 25);
+            this.levelSelect.SelectedIndexChanged += new System.EventHandler(this.levelSelect_IndexChange);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 349);
+            this.Controls.Add(this.outputAdb);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "frmMain";
+            this.Text = "Logcat #";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
 
 		}
 
 		#endregion
 
-		private System.Windows.Forms.TextBox textAdb;
+        private System.Windows.Forms.RichTextBox outputAdb;
 		private System.Windows.Forms.ToolStrip toolStrip1;
-		private System.Windows.Forms.ToolStripButton toolStripButtonClear;
+		private System.Windows.Forms.ToolStripButton btnClear;
 		private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
 		private System.Windows.Forms.ToolStripLabel toolStripLabel1;
-		private System.Windows.Forms.ToolStripTextBox toolStripTextBoxFilter;
-		private System.Windows.Forms.ToolStripComboBox toolStripComboBoxFilterType;
-		private System.Windows.Forms.ToolStripButton toolStripButtonStart;
+		private System.Windows.Forms.ToolStripTextBox txtFilter;
+		private System.Windows.Forms.ToolStripComboBox regSelect;
+		private System.Windows.Forms.ToolStripButton btnStart;
 		private System.Windows.Forms.ToolStripButton toolStripButtonStop;
 		private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripComboBox deviceSelect;
+        private System.Windows.Forms.ToolStripComboBox levelSelect;
 	}
 }
 
--- LogcatSharp/frmMain.cs
+++ LogcatSharp/frmMain.cs
@@ -24,7 +24,8 @@ namespace LogcatSharp
         private void frmMain_Load(object sender, EventArgs e)
         {
 
-            this.toolStripComboBoxFilterType.Text = "Simple";
+            this.regSelect.SelectedIndex = 0;
+            //this.levelSelect.SelectedIndex = 1;
 
             this.adbPath = Settings.GetPath();
 
@@ -44,6 +45,7 @@ namespace LogcatSharp
                     Application.Exit();
                 }
             }
+            devices();
         }
 
         void clear()
@@ -66,6 +68,38 @@ namespace LogcatSharp
 
         }
 
+        void devices()
+        {
+
+            if (adb != null && !adb.HasExited)
+                return;
+
+            adb = new Process();
+            adb.StartInfo.UseShellExecute = false;
+            adb.StartInfo.FileName = this.adbPath;
+            adb.StartInfo.Arguments = "devices";
+            adb.StartInfo.RedirectStandardOutput = true;
+            adb.StartInfo.RedirectStandardError = true;
+            adb.EnableRaisingEvents = true;
+            adb.StartInfo.CreateNoWindow = true;
+            //adb.ErrorDataReceived += new DataReceivedEventHandler(adb_ErrorDataReceived);
+            adb.OutputDataReceived += new DataReceivedEventHandler(adbDevices_OutputDataReceived);
+
+
+            try { var started = adb.Start(); }
+            catch (Exception ex)
+            {
+                this.btnStart.Enabled = true;
+                this.toolStripButtonStop.Enabled = false;
+
+                Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
+            }
+
+            //adb.BeginErrorReadLine();
+            adb.BeginOutputReadLine();
+
+        }
+
         void stop()
         {
             this.toolStripButtonStop.Enabled = false;
@@ -75,12 +109,54 @@ namespace LogcatSharp
                 adb.Kill();
             }
 
-            this.toolStripButtonStart.Enabled = true;
+            this.btnStart.Enabled = true;
         }
 
         void start()
         {
-            this.toolStripButtonStart.Enabled = false;
+            if (deviceSelect.Items.Count > 0)
+            {
+                if (deviceSelect.Items.Count == 1)
+                {
+                    deviceSelect.SelectedIndex = 0;
+                }
+                if (deviceSelect.SelectedItem == null)
+                {
+                    outputAdb.AppendText("Please select one devices\n");
+                    return;
+                }
+            }
+            else
+            {
+                outputAdb.AppendText("No devices connected\n");
+                return;
+            }
+
+            String device = deviceSelect.Text;
+            String logLevel = "D";
+            switch (this.levelSelect.SelectedIndex)
+            {
+                case 0:
+                    logLevel = "V";
+                    break;
+                case 1:
+                    logLevel = "D";
+                    break;
+                case 2:
+                    logLevel = "I";
+                    break;
+                case 3:
+                    logLevel = "W";
+                    break;
+                case 4:
+                    logLevel = "E";
+                    break;
+                case 5:
+                    logLevel = "F";
+                    break;
+            }
+
+            this.btnStart.Enabled = false;
 
             if (adb != null && !adb.HasExited)
                 return;
@@ -88,7 +164,7 @@ namespace LogcatSharp
             adb = new Process();
             adb.StartInfo.UseShellExecute = false;
             adb.StartInfo.FileName = this.adbPath;//
-            adb.StartInfo.Arguments = "logcat";
+            adb.StartInfo.Arguments = String.IsNullOrEmpty(device) ? "" : ("-s " + device + " logcat") + (" *:" + logLevel);
             adb.StartInfo.RedirectStandardOutput = true;
             adb.StartInfo.RedirectStandardError = true;
             adb.EnableRaisingEvents = true;
@@ -100,7 +176,7 @@ namespace LogcatSharp
             try { var started = adb.Start(); }
             catch (Exception ex)
             {
-                this.toolStripButtonStart.Enabled = true;
+                this.btnStart.Enabled = true;
                 this.toolStripButtonStop.Enabled = false;
 
                 Console.WriteLine(ex.Message + Environment.NewLine + ex.StackTrace);
@@ -112,12 +188,45 @@ namespace LogcatSharp
             this.toolStripButtonStop.Enabled = true;
         }
 
+        void adbDevices_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (this.InvokeRequired)
+                this.Invoke(new Action<object, DataReceivedEventArgs>(adbDevices_OutputDataReceived), sender, e);
+            else
+            {
+                String str = filterData(e.Data);
+                if (str.Contains("\tdevice"))
+                {
+                    this.deviceSelect.Items.Add(str.Replace("\tdevice", "").Replace("\r","").Replace("\n",""));
+                }
+                outputAdb.AppendText(filterData(e.Data));
+            }
+        }
         void adb_OutputDataReceived(object sender, DataReceivedEventArgs e)
         {
             if (this.InvokeRequired)
                 this.Invoke(new Action<object, DataReceivedEventArgs>(adb_OutputDataReceived), sender, e);
             else
-                textAdb.AppendText(filterData(e.Data));
+            {
+                if (String.IsNullOrEmpty(e.Data))
+                {
+                    return;
+                }
+                string str = e.Data;
+                string color = "0x000000";
+                if (str.IndexOf(" V ") != -1)
+                {
+                    color = "0xBBBBBB";
+                }
+                else if (str.Contains(" D ")) { color = "0x0070BB"; }
+                else if (str.Contains(" I ")) { color = "0x48BB31"; }
+                else if (str.Contains(" W ")) { color = "0xBBBB23"; }
+                else if (str.Contains(" E ")) { color = "0xFF0006"; }
+                else if (str.Contains(" A ")) { color = "0x8F0005"; }
+
+                outputAdb.SelectionColor = ColorTranslator.FromHtml(color);
+                outputAdb.AppendText(filterData(str));
+            }
         }
 
         void adb_ErrorDataReceived(object sender, DataReceivedEventArgs e)
@@ -125,12 +234,15 @@ namespace LogcatSharp
             if (this.InvokeRequired)
                 this.Invoke(new Action<object, DataReceivedEventArgs>(adb_ErrorDataReceived), sender, e);
             else
-                textAdb.AppendText(filterData(e.Data));
+            {
+                outputAdb.SelectionColor = Color.Red; 
+                outputAdb.AppendText(filterData(e.Data));
+            }
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
-            textAdb.Clear();
+            outputAdb.Clear();
             //stop(); by jeo
             //clear();
             //start(); by jeo
@@ -141,10 +253,10 @@ namespace LogcatSharp
             if (string.IsNullOrWhiteSpace(data))
                 return string.Empty;
 
-            if (!string.IsNullOrEmpty(this.toolStripTextBoxFilter.Text))
+            if (!string.IsNullOrEmpty(this.txtFilter.Text))
             {
-                var pattern = !this.toolStripComboBoxFilterType.Text.Equals("Regex") ? this.toolStripTextBoxFilter.Text.Replace("*", ".*")
-                    : this.toolStripTextBoxFilter.Text;
+                var pattern = !this.regSelect.Text.Equals("Regex") ? this.txtFilter.Text.Replace("*", ".*")
+                    : this.txtFilter.Text;
                 System.Text.RegularExpressions.Regex rx = new System.Text.RegularExpressions.Regex(pattern, System.Text.RegularExpressions.RegexOptions.Singleline | System.Text.RegularExpressions.RegexOptions.IgnoreCase);
 
                 if (rx.IsMatch(data))
@@ -164,6 +276,13 @@ namespace LogcatSharp
         private void toolStripButtonStop_Click(object sender, EventArgs e)
         {
             stop();
+        }
+
+        private void levelSelect_IndexChange(object sender, EventArgs e)
+        {
+            stop();
+            start();
+
         }
     }
 }
